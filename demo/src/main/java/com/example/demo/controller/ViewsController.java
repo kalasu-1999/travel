55a0d5b1488f0e019b45a6756d4f46ea741c00b5
kalasu-1999 <git@github.com:kalasu-1999/Java.git>
2020-07-17 18:48:55 +0800

    demo01

--- demo/src/main/java/com/example/demo/controller/ViewsController.java
+++ demo/src/main/java/com/example/demo/controller/ViewsController.java
@@ -112,8 +112,8 @@ public class ViewsController {
     @RequestMapping("/deleteViewsByViewId")
     public Map<String, Object> deleteViewsByViewId(Integer viewId) {
         Map<String, Object> map = new HashMap<>();
+        viewLineService.deleteViewLineByViewId(viewId);
         if (viewsService.deleteViewsByViewId(viewId) == 1) {
-            viewLineService.deleteViewLineByViewId(viewId);
             map.put("code", 0);
             map.put("msg", "景点信息删除成功");
         } else {
